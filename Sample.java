aaf15aa7edd26b46cdede607143d96cc81129201
Oushnik20 <137035839+Oushnik20@users.noreply.github.com>
2025-01-24 14:29:55 +0530

    second commit

--- Sample.java
+++ Sample.java
@@ -1,7 +1,11 @@
 class Sample{
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("World!");
     }
 }
 
 // . means that path
+// ssh-keygen
+// github - settings - ssh new sshh key - key paste it
+// title Windows Oushnik 
